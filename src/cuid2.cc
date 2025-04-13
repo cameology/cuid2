e332345dd745934cd914161a35b67b20d9f6d62d
Rheza A <rheza.as@gmail.com>
2025-04-13 20:22:35 +0700

    free bignum when creating cuid

--- src/cuid2.cc
+++ src/cuid2.cc
@@ -1,6 +1,7 @@
 #include "cuid2/cuid2.h"
 
 #include <string>
+#include <openssl/evp.h>
 
 #include "cuid2/utils.h"
 #include "cuid2/contants.h"
@@ -10,13 +11,22 @@
 std::string cuid2::createId() {
     std::string cuid2    = "";
 
+    BIGNUM* epoch   = BN_new();
+    BIGNUM* counter = BN_new();
+
+    BN_set_word(epoch, cuid2::epochInNano());
+    BN_set_word(counter, cuid2::counter());
+
     cuid2 += cuid2::alpha();
     cuid2 += cuid2::hash(""
-            + cuid2::base36Encode(cuid2::toBignum(cuid2::epochInNano()))    // base36 time
-            + cuid2::entropy(cuid2::DEFAULT_LENGTH)                         // salt
-            + cuid2::base36Encode(cuid2::toBignum(cuid2::counter()))        // base36 counter
-            + cuid2::fingerprint()                                          // fingerprint
+            + cuid2::base36Encode(epoch)                // base36 time
+            + cuid2::entropy(cuid2::DEFAULT_LENGTH)     // salt
+            + cuid2::base36Encode(counter)              // base36 counter
+            + cuid2::fingerprint()                      // fingerprint
         );
+
+    BN_free(epoch);
+    BN_free(counter);
 
     return cuid2.substr(1, cuid2::DEFAULT_LENGTH);
 }
