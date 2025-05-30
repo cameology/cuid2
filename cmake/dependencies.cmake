f1a8b4a647ce2931f6114b9135e8dd49600dee61
Rheza A <rheza.as@gmail.com>
2025-05-30 14:17:51 +0700

    separate test dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -2,15 +2,17 @@ include(FetchContent)
 Set(FETCHCONTENT_QUIET false)
 
 
-# GoogleTest
-FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG        v1.16.0
-)
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
-
 # OpenSSL
 find_package(OpenSSL REQUIRED)
+
+if(ENABLE_TESTS)
+    # GoogleTest
+    FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG        v1.16.0
+    )
+    # For Windows: Prevent overriding the parent project's compiler/linker settings
+    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+    FetchContent_MakeAvailable(googletest)
+endif()
